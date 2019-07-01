f28dcc1c08ab9f386f12dd036f13fe94ae2a29f9
redashu <ashutoshh@linux.com>
2019-07-01 23:49:36 +0530

    computer visison

---
+++ red_color.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import  cv2
+#  start  camera
+cap=cv2.VideoCapture(2)
+#  status of camera
+while  cap.isOpened():
+    status,frame=cap.read()
+#   detecting  red color
+    redimg=cv2.inRange(frame,(0,0,0),(40,40,255))  
+    cv2.imshow('liveredcolor',redimg)
+    if  cv2.waitKey(10)  & 0xff  == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+cap.release()
+
